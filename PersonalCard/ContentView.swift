d11df4b665c3a65aa615c4b1b96bf2062d3ed528
SelenaGarcia <selenagarcialobo@gmail.com>
2022-07-23 12:29:11 -0300

    Initial Commit

---
+++ PersonalCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PersonalCard
+//
+//  Created by Selena Belén Garcia Lobo on 23/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PersonalCard/PersonalCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalCardApp.swift
+//  PersonalCard
+//
+//  Created by Selena Belén Garcia Lobo on 23/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct PersonalCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
